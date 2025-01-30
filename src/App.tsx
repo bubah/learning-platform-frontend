f26bf0330523669721887bef97983b50d42a7290
Bubah Conteh <contehbub@gmail.com>
2025-01-29 20:42:31 -0500

    BC | Adds Course List page

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,14 @@
 import "./App.css";
+import { CourseList } from "./components/Course/CourseListPage";
 import { CoursePage } from "./components/Course/CoursePage";
-import { Curriculum } from "./components/Course/Curriculum";
-import { EditCourse } from "./components/Course/EditCourse";
 import CourseManagementScreen from "./components/Course/MangeCoursesPage";
-import UpdateAttributeField from "./components/Course/UpdateAttributeFied";
-import { NavBar } from "./components/NavBar";
 function App() {
   return (
     <div style={{ minWidth: "650px" }}>
       {/* <NavBar /> */}
       {/* <CoursePage /> */}
-      <CourseManagementScreen />
-      {/* <UpdateAttributeField /> */}
-      {/* <EditCourse /> */}
-      {/* <Curriculum /> */}
+      {/* <CourseManagementScreen /> */}
+      <CourseList/>
     </div>
   );
 }
---
+++ src/components/Course/CourseListPage.tsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+import { Course, mockCourse } from "../../mock-data/course";
+import { Box, Card, CardMedia, Typography } from "@mui/material";
+
+export const CourseList = () => {
+  const [corses, setCourses] = useState<Course[] | []>([]);
+
+  useEffect(() => {
+    setCourses([mockCourse, mockCourse]);
+  });
+  return (
+    <Box sx={{ margin: 10 }}>
+      {corses.map((course) => (
+        <Card variant="outlined" sx={{ marginBottom: 5, display: "flex" }}>
+          <Box>
+            <CardMedia
+              component="img"
+              sx={{ height: 150 }}
+              image="https://picsum.photos/150/150"
+              alt="green iguana"
+            />
+          </Box>
+          <Box sx={{ flex: 1 }}>
+            <Typography>{course.title}</Typography>
+          </Box>
+          <Box sx={{ flex: 1 }}>
+            <Typography>Meta Data</Typography>
+          </Box>
+        </Card>
+      ))}
+    </Box>
+  );
+};
