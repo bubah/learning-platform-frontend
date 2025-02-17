00b2bc4b42f63869a3dbfd21a795e92fccf6439a
hassanrazak <76017055+hassanrazak@users.noreply.github.com>
2025-02-16 22:04:29 -0500

    section edit and save crud connected to backend

--- src/components/Course/SectionComponent.tsx
+++ src/components/Course/SectionComponent.tsx
@@ -10,6 +10,7 @@ import { useState } from "react";
 
 import { memo } from "react";
 import UpdateAttributeFeild from "./UpdateAttributeFied";
+import axios from "axios";
 
 export const SectionComponent = memo(
   ({
@@ -23,15 +24,34 @@ export const SectionComponent = memo(
     title: string;
     id:string
   }) => {
-    const [sectionState, setSectionState] = useState<{
-      title: string;
-      description: string;
-    }>({ title, description });
+
+    const[sectionTitle,setSectionTitle] = useState<string>(title);
+    const[sectionDescription, setSectionDescription] = useState<string>(description);
+
     const [isAddingTitle, setIsAddingTitle] = useState(false);
     const [isAddingDescription, setIsAddingDescription] = useState(false);
 
-    const handleSaveV2 = (value: string) => {
+    const updateSectionTitle = (value: string) => {
       console.log("Printing value", value);
+      console.log(id);
+      const requestBody = {title:value};
+      axios.put(`http://localhost:8080/sections/${id}`,requestBody)
+      .then((res) => {
+        const {updatedTitle} = res.data.title;
+        console.log(updatedTitle);
+        setSectionTitle(updatedTitle);
+      })
+    };
+
+    const updateSectionDescription = (value: string) => {
+      console.log("Printing value", value);
+      const requestBody = {description:value};
+      console.log(id);
+      axios.put(`http://localhost:8080/sections/${id}`,requestBody)
+      .then((res) => {
+        const {description} = res.data.description;
+        setSectionDescription(description);
+      })
     };
 
     return (
@@ -46,17 +66,17 @@ export const SectionComponent = memo(
         >
           <CardContent>
             <UpdateAttributeFeild
-              attributeValue={sectionState.title}
+              attributeValue={sectionTitle}
               inEditing={isAddingTitle}
               label="Title"
-              handleUpdate={handleSaveV2}
+              handleUpdate={updateSectionTitle}
             />
 
             <UpdateAttributeFeild
-              attributeValue={sectionState.description}
+              attributeValue={sectionDescription}
               inEditing={isAddingDescription}
               label="Description"
-              handleUpdate={handleSaveV2}
+              handleUpdate={updateSectionDescription}
             />
 
             <TextField
