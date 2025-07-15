03ffe6764e9215e3396da98962f83bae749a1edc
hassanrazak <76017055+hassanrazak@users.noreply.github.com>
2025-07-15 13:02:41 -0400

    fixing linting issue

--- src/pages/shared/LandingPage.tsx
+++ src/pages/shared/LandingPage.tsx
@@ -1,4 +1,4 @@
-import { Box, Typography } from "@mui/material";
+import { Box } from "@mui/material";
 import { ImageCarousel } from "../../components/shared/ImageCarousel";
 import { FeaturedCourses } from "../../components/shared/FeaturedCourses";
 
